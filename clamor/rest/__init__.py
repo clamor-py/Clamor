2a846649ad023fc67b171708161c705693c5ef06
Valentin B <valentin.be@protonmail.com>
2019-07-19 15:14:24 +0200

    Add wrappers for User and Webhook

--- clamor/rest/__init__.py
+++ clamor/rest/__init__.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from . import endpoints
 from .http import *
 from .rate_limit import *
 from .routes import *
--- clamor/rest/endpoints/__init__.py
+++ clamor/rest/endpoints/__init__.py
@@ -5,4 +5,6 @@ from .audit_log import *
 from .channel import *
 from .emoji import *
 from .invite import *
+from .user import *
 from .voice import *
+from .webhook import *
